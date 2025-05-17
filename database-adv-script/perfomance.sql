98d32807d2d8c20f685358e32612c741b8ca7a3a
ALASSANE Fadeilou <68302531+Fadeilou@users.noreply.github.com>
2025-05-17 23:35:34 +0100

    Create perfomance.sql

---
+++ database-adv-script/perfomance.sql
@@ -0,0 +1,6 @@
+-- Version initiale non optimisée
+SELECT b.id, u.name, p.name, pay.amount
+FROM bookings b
+JOIN users u ON b.user_id = u.id
+JOIN properties p ON b.property_id = p.id
+JOIN payments pay ON pay.booking_id = b.id;
